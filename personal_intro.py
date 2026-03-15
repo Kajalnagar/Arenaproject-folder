85c2436d273f1a8f623665a6fb04441583f198a2
Kajalnagar <nagarkajal420@gmail.com>
2026-03-15 17:35:33 +0530

    Add personal introduction program

---
+++ personal_intro.py
@@ -0,0 +1,19 @@
+
+# Week 1 Internship Project
+# Personal Introduction Program
+
+# Ask the user for their information
+name = input("What is your name? ")
+age = input("How old are you? ")
+hobby = input("What is your favorite hobby? ")
+city = input("Which city do you live in? ")
+
+# Display a friendly welcome message
+print("\n")
+print(" Welcome", name + "! ")
+print("Nice to meet you.")
+print("You are", age, "years old.")
+print("You enjoy", hobby + ".")
+print("And you live in", city + ".")
+print("Have a great day!")
+print("\n")
